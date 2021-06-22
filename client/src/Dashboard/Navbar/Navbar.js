0cd03569bdc0fd637a66b36c46fc296d8a76a06f
Layne Rasmussen <layrasm@gmail.com>
2021-06-21 18:41:35 -0600

    mobile responsiveness for navbar fixed

--- client/src/Dashboard/Navbar/Navbar.js
+++ client/src/Dashboard/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+
+
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 const NavBar = () => {
 
@@ -7,11 +9,24 @@ const NavBar = () => {
 
 
   return (
-    <Navbar>
-      <Navbar.Collapse id="basic-navbar-nav">
-        <h1>Dashboard</h1>
-        <p>{today}</p>
-        <h1>Hello</h1>
+    <Navbar expand="md" >
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <h1>Dashboard</h1>
+      <p>{today}</p>
+      <Navbar.Collapse id="basic-navbar-nav" >
+        <Nav className="mr-auto">
+
+          <Nav.Link href="#home">Home</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+
+
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
   )
