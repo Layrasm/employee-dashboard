5f3bdbd477c336f8664642d3b06d87a6fb65f346
Layne Rasmussen <layrasm@gmail.com>
2021-06-25 01:06:46 -0600

    added states for cards for task lists

--- client/src/Dashboard/Tasks/CreateTask.js
+++ client/src/Dashboard/Tasks/CreateTask.js
@@ -1,10 +1,29 @@
 import React, { useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 
-const CreateTask = ({ show, handleClose }) => {
+const CreateTask = ({ show, handleClose, addToList }) => {
   const [taskName, setTaskName] = useState('');
   const [description, setDescription] = useState('');
 
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    e.preventDefault();
+
+    if (name === "taskName") {
+      setTaskName(value)
+    } else {
+      setDescription(value)
+    }
+  }
+
+  const handleSave = () => {
+    let taskObj = {}
+    taskObj["Name"] = taskName
+    taskObj["Description"] = description
+    addToList(taskObj)
+    handleClose()
+  }
 
 
   return (
@@ -15,17 +34,17 @@ const CreateTask = ({ show, handleClose }) => {
         <Form>
           <Form.Group>
             <Form.Label>Task</Form.Label>
-            <Form.Control as="textarea" rows={1} value={taskName} />
+            <Form.Control as="textarea" rows={1} value={taskName} onChange={handleChange} name="taskName" />
           </Form.Group>
           <Form.Group>
             <Form.Label>Description</Form.Label>
-            <Form.Control as="textarea" rows={3} value={description} />
+            <Form.Control as="textarea" rows={3} value={description} onChange={handleChange} name="taskDescription" />
           </Form.Group>
         </Form>
       </Modal.Body>
       <Modal.Footer style={{ border: "none" }}>
         <Button color="secondary" onClick={handleClose}>Cancel</Button>
-        <Button color="primary" onClick={handleClose}>Create</Button>
+        <Button color="primary" onClick={handleSave}>Create</Button>
 
       </Modal.Footer>
     </Modal>
--- client/src/Dashboard/Tasks/Tasks.js
+++ client/src/Dashboard/Tasks/Tasks.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Card } from 'react-bootstrap';
 import './Tasks.css';
 import CreateTask from './CreateTask';
 
 const Tasks = () => {
 
   const [show, setShow] = useState(false);
+  const [allTasks, setAllTasks] = useState([]);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const addToList = (task) => {
+    let updateList = allTasks
+    updateList.push(task);
+    setAllTasks(updateList)
+  }
 
   return (
     <div>
@@ -18,7 +25,17 @@ const Tasks = () => {
       <Container>
         <Button className="taskButton" onClick={handleShow}>New Task</Button>
       </Container>
-      <CreateTask show={show} handleClose={handleClose} />
+      <div style={{ display: "flex" }}>
+        {allTasks.map((obj) =>
+          <Card style={{ width: '20rem', }}>
+            <Card.Header>{obj.Name}</Card.Header>
+            <Card.Body >
+              <Card.Text className="hide-native-scrollbar" style={{ height: '10rem', overflowY: 'scroll' }}>{obj.Description}</Card.Text>
+            </Card.Body>
+          </Card>
+        )}
+      </div>
+      <CreateTask show={show} handleClose={handleClose} addToList={addToList} />
     </div >
   )
 }
