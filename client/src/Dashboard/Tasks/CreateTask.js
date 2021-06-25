66aff952e722b4aff1658a7d020bc3b3482f180e
Layne Rasmussen <layrasm@gmail.com>
2021-06-24 23:46:45 -0600

    added form to task modal

--- client/src/Dashboard/Tasks/CreateTask.js
+++ client/src/Dashboard/Tasks/CreateTask.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
 
 const CreateTask = ({ show, handleClose }) => {
+  const [taskName, setTaskName] = useState('');
+  const [description, setDescription] = useState('');
+
+
 
   return (
 
     <Modal show={show} onHide={handleClose}>
-      <Modal.Header>Add Task</Modal.Header>
+      <Modal.Header>Add Task +</Modal.Header>
       <Modal.Body>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </Modal.Body>
-      <Modal.Footer>
+        <Form>
+          <Form.Group>
+            <Form.Label>Task</Form.Label>
+            <Form.Control as="textarea" rows={1} value={taskName} />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Description</Form.Label>
+            <Form.Control as="textarea" rows={3} value={description} />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer style={{ border: "none" }}>
         <Button color="secondary" onClick={handleClose}>Cancel</Button>
-        <Button color="primary" onClick={handleClose}>Do Something</Button>{' '}
+        <Button color="primary" onClick={handleClose}>Create</Button>
 
       </Modal.Footer>
     </Modal>
